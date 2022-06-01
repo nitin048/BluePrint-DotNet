ed64e744e04dc01f0bb842bb2a8568289b666c7b
pankajsakhare19 <pankajsakhare91@gmail.com>
2022-06-01 14:07:14 +0530

    Initial Structure Setup

---
+++ BluePrintCore/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluePrintCore.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
